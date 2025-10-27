fa271575f18490640eb9ef70b0a203034d3deae4
youngnam81-kim <kimyn81@naver.com>
2025-10-27 11:26:27 +0900

    Initial commit

---
+++ src/main/java/com/kim/ex25_branch/Ex25BranchApplication.java
@@ -0,0 +1,13 @@
+package com.kim.ex25_branch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ex25BranchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ex25BranchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kim/ex25_branch/Ex25BranchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kim.ex25_branch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ex25BranchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
